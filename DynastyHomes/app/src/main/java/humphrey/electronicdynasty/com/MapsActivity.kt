9e96a192a2347e21a27647c8629d5ef8c1375f91
humphreybrian <developer.humphrey@gmail.com>
2021-04-15 21:52:09 +0300

    added fine current location for user

--- DynastyHomes/app/src/main/java/humphrey/electronicdynasty/com/MapsActivity.kt
+++ DynastyHomes/app/src/main/java/humphrey/electronicdynasty/com/MapsActivity.kt
@@ -1,7 +1,10 @@
 package humphrey.electronicdynasty.com
 
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -10,12 +13,21 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private lateinit var map: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    //getting current location
+    private lateinit var lastLocation: Location
+
+    //permission to request for location
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+        private const val REQUEST_CHECK_SETTINGS = 2
+        private const val PLACE_PICKER_REQUEST = 3
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,21 +51,46 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
      */
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
-
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-1.286389, 36.817223)
-        map.addMarker(MarkerOptions().position(sydney).title("Marker in Nairobi"))
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-//        val myPlace = LatLng(40.73, -73.99)  // this is New York
-//        map.addMarker(MarkerOptions().position(myPlace).title("My Favorite City"))
-//        map.moveCamera(CameraUpdateFactory.newLatLng(myPlace))
-        map.getUiSettings().setZoomControlsEnabled(true)
+//        val sydney = LatLng(-1.286389, 36.817223)
+//        map.addMarker(MarkerOptions().position(sydney).title("Marker in Nairobi"))
+//        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+////        val myPlace = LatLng(40.73, -73.99)  // this is New York
+////        map.addMarker(MarkerOptions().position(myPlace).title("My Favorite City"))
+////        map.moveCamera(CameraUpdateFactory.newLatLng(myPlace))
+//        map.getUiSettings().setZoomControlsEnabled(true)
+//        map.setOnMarkerClickListener(this)
+//        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 12.0f))
+        map.uiSettings.isZoomControlsEnabled = true
         map.setOnMarkerClickListener(this)
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 12.0f))
+        setUpMap()
+
+    }
+    override fun onMarkerClick(p0: Marker?)=false
+
+
+    //HERE WE CHECK FINE LOCATION PERMISSIONS GRANTED INTO THE APP.
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+        map.isMyLocationEnabled = true
+        map.mapType = GoogleMap.MAP_TYPE_NORMAL
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+            // Got last known location. In some rare situations this can be null.
+            if (location != null){
+                lastLocation = location
+                val currentLatLng = LatLng (location.latitude, location.longitude)
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+            }
+        }
     }
 
-//    override fun onMarkerClick(p0: Marker?): Boolean {
-//        TODO("Not yet implemented")
-//    }
-    override fun onMarkerClick(p0: Marker?)=false;
+
+
+
+
 }
